1c08438f72791507ef0d4c74ff34e4539b8185d7
prince7024 <princekumarchoudhary22@gmail.com>
2026-04-03 16:42:44 +0530

    url update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://skillconnect-backend-3e6v.onrender.com/api", 
+  baseURL: import.meta.env.VITE_API_URL, 
 });
 
 API.interceptors.request.use((config) => {
